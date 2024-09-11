449d59986c1f6ad025e5393a8bf29235dd25e672
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-09-11 12:50:34 +0100

    file upload kinda works, but not really

--- components/FileUpload.tsx
+++ components/FileUpload.tsx
@@ -1,12 +1,12 @@
 'use client'
 
 import { useState } from 'react';
-import { createClientComponentClient } from '../utils/supabase/client';
+import { createClient } from '../utils/supabase/client';
 
 export default function FileUpload() {
   const [file, setFile] = useState<File | null>(null);
   const [uploading, setUploading] = useState(false);
-  const supabase = createClientComponentClient();
+  const supabase = createClient();
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
