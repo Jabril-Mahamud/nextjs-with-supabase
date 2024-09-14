b8517fae8299cb347c14b960c3a63e392df4b485
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-09-14 01:53:13 +0100

    removed white background

--- app/uploads/page.tsx
+++ app/uploads/page.tsx
@@ -7,7 +7,7 @@ const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen p-4 ">
       <h1 className="text-2xl md:text-3xl font-bold mb-8">Your Uploaded Files</h1>
-      <div className="w-full max-w-3xl border shadow-md rounded-lg p-4 bg-white">
+      <div className="w-full max-w-3xl border shadow-md rounded-lg p-4">
         <FileList />
       </div>
     </div>
